575cefe101d1fcd5d807e8d451182fe71a136d64
Eric van Wijk <eric@van-wijk.com>
2023-10-28 14:55:13 +0200

    Revert "Initial experiment"

--- environment-samples/storage-account/outputs.tf
+++
@@ -1,7 +0,0 @@
-output blob_url {
-  value                        = azurerm_storage_account.storage.primary_blob_endpoint
-}
-
-output resource_group_portal_url {
-  value                        = "https://portal.azure.com/#@${data.azurerm_client_config.current.tenant_id}/resource${azurerm_resource_group.storage.id}/overview"
-}
